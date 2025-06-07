ab0e24f8bf04027ac426cb7386a2126e2c5aebf4
oderlive <art20012004@gmail.com>
2025-06-07 17:05:55 +0300

    fix store

--- vue-accounts-test/src/main.ts
+++ vue-accounts-test/src/main.ts
@@ -4,8 +4,12 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 const app = createApp(App)
-app.use(createPinia())
+app.use(pinia)
 app.use(ElementPlus)
 app.mount('#app')
