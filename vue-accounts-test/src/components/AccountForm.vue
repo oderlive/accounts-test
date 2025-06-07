cf2e5c910853ea2d9c0bef12a887c94192c423e3
oderlive <art20012004@gmail.com>
2025-06-07 17:05:40 +0300

    fix style and fix as in the layout style

--- vue-accounts-test/src/components/AccountForm.vue
+++ vue-accounts-test/src/components/AccountForm.vue
@@ -1,21 +1,98 @@
 <template>
   <div>
-    <h1>Учетные записи</h1>
-    <el-button type="primary" @click="addAccount">+</el-button>
-    <p>Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;</p>
-    <AccountRow
-        v-for="(acc, index) in store.accounts"
-        :key="index"
-        :account="acc"
-        :index="index"
-    />
+    <div class="header">
+      <div class="title">Учетные записи</div>
+      <el-button type="primary" @click="addAccount" size="large">+</el-button>
+    </div>
+    <div class="help" title="Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;">
+      <HelpCircle class="help-icon" />
+      <span class="help-text">Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;</span>
+    </div>
+    <table class="accounts-table">
+      <thead>
+      <tr>
+        <th>Метки</th>
+        <th>Тип записи</th>
+        <th>Логин</th>
+        <th>Пароль</th>
+      </tr>
+      </thead>
+      <tbody>
+      <AccountRow
+          v-for="(acc, index) in store.accounts"
+          :key="index"
+          :account="acc"
+          :index="index"
+      />
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useAccountStore } from '../store/account'
 import AccountRow from './AccountRow.vue'
+import { HelpCircle } from 'lucide-vue-next'
 
 const store = useAccountStore()
 const addAccount = () => store.addAccount()
 </script>
+
+<style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+
+.title {
+  font-size: 2em;
+}
+
+.accounts-table {
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 10px 10px;
+  margin-top: 20px;
+}
+
+th,
+td {
+  border: none;
+  padding: 10px;
+  text-align: left;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+th {
+  background-color: transparent;
+  text-align: left;
+  font-weight: 400;
+  color: gray;
+}
+
+.help {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  background-color: aliceblue;
+  padding: 5px 10px;
+  margin-top: 20px;
+  border-radius: 5px;
+  cursor: help;
+}
+
+.help-icon {
+  width: 16px;
+  height: 16px;
+  margin-right: 5px;
+  color: #888;
+}
+
+.help-text {
+  font-size: 0.9em;
+  color: #555;
+}
+</style>
--- vue-accounts-test/src/components/AccountRow.vue
+++ vue-accounts-test/src/components/AccountRow.vue
@@ -1,39 +1,52 @@
 <template>
-  <div class="account-row">
-    <el-input
-        placeholder="Метки"
-        v-model="metkiRaw"
-        @blur="validateMetki"
-        :maxlength="50"
-    />
-    <el-select v-model="local.type" @change="onTypeChange">
-      <el-option label="Локальная" value="Локальная" />
-      <el-option label="LDAP" value="LDAP" />
-    </el-select>
-    <el-input
-        placeholder="Логин"
-        v-model="local.login"
-        @blur="validateLogin"
-        :maxlength="100"
-        :class="{ error: !valid.login }"
-    />
-    <el-input
-        v-if="local.type === 'Локальная'"
-        placeholder="Пароль"
-        v-model="local.password"
-        show-password
-        @blur="validatePassword"
-        :maxlength="100"
-        :class="{ error: !valid.password }"
-    />
-    <el-button type="danger" @click="remove">Удалить</el-button>
-  </div>
+  <tr>
+    <td>
+      <el-input
+          placeholder="Метки"
+          v-model="metkiRaw"
+          @blur="validateMetki"
+          :maxlength="50"
+      />
+    </td>
+    <td>
+      <el-select v-model="local.type" @change="onTypeChange">
+        <el-option label="Локальная" value="Локальная" />
+        <el-option label="LDAP" value="LDAP" />
+      </el-select>
+    </td>
+    <td :colspan="local.type === 'LDAP' ? 2 : 1">
+      <el-input
+          placeholder="Логин"
+          v-model="local.login"
+          @blur="validateLogin"
+          :maxlength="100"
+          :class="{ error: !valid.login }"
+      />
+    </td>
+    <td v-if="local.type === 'Локальная'">
+      <el-input
+          v-if="local.type === 'Локальная'"
+          placeholder="Пароль"
+          :type="passwordInputType"
+          v-model="local.password"
+          @blur="validatePassword"
+          :maxlength="100"
+          :class="{ error: !valid.password }"
+          :style="{ 'text-overflow': 'ellipsis', 'overflow': 'hidden', 'white-space': 'nowrap' }"
+          :show-password="true"
+      />
+    </td>
+    <td>
+      <Trash2 class="cursor-pointer text-red-500 trash-icon" @click="remove" />
+    </td>
+  </tr>
 </template>
 
 <script setup lang="ts">
-import {reactive, watch, toRefs, ref} from 'vue'
+import { reactive, watch, toRefs, ref, computed } from 'vue'
 import { useAccountStore } from '../store/account'
 import type { Account } from '../types/accounts.ts'
+import { Trash2 } from 'lucide-vue-next'
 
 const props = defineProps<{ account: Account, index: number }>()
 const store = useAccountStore()
@@ -68,18 +81,24 @@ function onTypeChange() {
     local.password = null
   }
 }
+
+const passwordInputType = computed(() => {
+  return local.type === 'Локальная' && local.password ? 'password' : 'text';
+});
+
 function remove() {
   store.removeAccount(props.index)
 }
 </script>
 
 <style scoped>
-.account-row {
-  display: flex;
-  gap: 10px;
-  margin-top: 10px;
-}
 .error input {
   border-color: red !important;
+}
+.trash-icon {
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  align-self: center;
 }
 </style>
